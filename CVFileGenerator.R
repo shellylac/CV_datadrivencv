357ff1bfdaa4939ebbadb5c27e5ecb8394ee031d
shellylac <shellstar@gmail.com>
2020-10-24 13:12:50 +0100

    updated file locations

--- CVFileGenerator.R
+++ CVFileGenerator.R
@@ -5,9 +5,7 @@ devtools::install_github("nstrayer/datadrivencv")
 datadrivencv::use_datadriven_cv(
   full_name = "Shelly Lachish",
   data_location = "https://docs.google.com/spreadsheets/d/1oHyUQ4dA_e5G2WHl8p2nszinrBz6gKxsR5Xve_P09Gs",
-  #pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
-  #html_location = "nickstrayer.me/cv/",
-  #source_location = "https://github.com/nstrayer/cv"
+  pdf_location = "https://github.com/shellylac/CV_datadrivencv/ShellyL_CV.pdf",
+  html_location = "https://github.com/shellylac/CV_datadrivencv/",
+  source_location = "https://github.com/shellylac/CV_datadrivencv"
 )
-
-?datadrivencv::use_datadriven_cv
