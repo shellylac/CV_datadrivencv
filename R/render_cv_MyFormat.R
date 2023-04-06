f85b7cff470ab2883accdfc584aa15ad013458b4
Shelly Lachish <shellstar@gmail.com>
2023-04-06 10:08:54 +1000

    updates files for 2023 CV format

--- R/render_cv_MyFormat.R
+++ R/render_cv_MyFormat.R
@@ -2,13 +2,13 @@ library(here)
 # This script builds both the HTML and PDF versions of your CV
 
 # Knit the HTML version
-#rmarkdown::render(here("R","cv_MyFormat.rmd"),
-#                  params = list(pdf_mode = FALSE),
-#                  output_file = here("cv_versions" , "ShellyL_CV.html"))
-
-rmarkdown::render(here("R","cv_MyFormat_csiro.rmd"),
+rmarkdown::render(here("R","cv_MyFormat.rmd"),
                   params = list(pdf_mode = FALSE),
-                  output_file = here("cv_versions" , "ShellyL_CV_csiro.html"))
+                  output_file = here("cv_versions" , "ShellyL_CV.html"))
+
+# rmarkdown::render(here("R","cv_MyFormat_csiro.rmd"),
+#                   params = list(pdf_mode = FALSE),
+#                   output_file = here("cv_versions" , "ShellyL_CV_csiro.html"))
 
 # Knit the PDF version to temporary html location -
 
@@ -17,18 +17,18 @@ rmarkdown::render(here("R","cv_MyFormat_csiro.rmd"),
 #* @ line 7 in the custom css file 
 
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
-#rmarkdown::render(here("R","cv_MyFormat.rmd"),
-#                  params = list(pdf_mode = TRUE),
-#                  output_file = tmp_html_cv_loc)
-
-#1 page
-rmarkdown::render(here("R","cv_MyFormat_csiro.rmd"),
+rmarkdown::render(here("R","cv_MyFormat.rmd"),
                   params = list(pdf_mode = TRUE),
                   output_file = tmp_html_cv_loc)
 
-# Convert to PDF using Pagedown
-#pagedown::chrome_print(input = tmp_html_cv_loc,
-#                       output = here("cv_versions", "ShellyL_CV.pdf"))
+# 
+# rmarkdown::render(here("R","cv_MyFormat_csiro.rmd"),
+#                   params = list(pdf_mode = TRUE),
+#                   output_file = tmp_html_cv_loc)
 
+# Convert to PDF using Pagedown
 pagedown::chrome_print(input = tmp_html_cv_loc,
-                       output = here("cv_versions", "ShellyL_CV_csiro.pdf"))
+                       output = here("cv_versions", "ShellyL_CV.pdf"))
+
+# pagedown::chrome_print(input = tmp_html_cv_loc,
+#                        output = here("cv_versions", "ShellyL_CV_csiro.pdf"))
